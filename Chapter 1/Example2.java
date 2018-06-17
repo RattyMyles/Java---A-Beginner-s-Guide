09fce8dc6910bc4a6efbbd2b2a88ae3d0704e2e4
RattyMyles <up734233@myport.ac.uk>
2018-06-17 23:21:49 +0100

    Add files via upload

--- Chapter 1/Example2.java
+++ Chapter 1/Example2.java
@@ -2,7 +2,7 @@
  *  Author: Rattymyles
  *  Chapter 1 Fundamentals of Java - A Beginner's Guide 6th Edition.
  *  The purpose of this project will breakdown each line of code to help fellow students and readers.
- *  This project will demonstrate if loops, while loops and for loops.
+ *  This project will demonstrate if loops.
  *  Any feedback or comments, please email my address as m.pucknell@gmail.com.
  */
 
@@ -36,7 +36,30 @@ public class Example2 {
 		if(c >= 0)System.out.println("c is non-negative");
 		if(c < 0)System.out.println("c is negative");
 
+		/*
+		 * For loops
+		 * Example: For how many values within that condition is met.
+		 */
+		int count;
+		//Instead of writing a loop on one line, you can create code blocks.
+		// The "{}" will hold the code you wish to execute within that loop. 
+		for(count = 0; count < 5; count = count+1)
+		{
+			System.out.println("This is count: " + count);
+		}
 		
+		/*
+		 * While loops 
+		 * While loops are good for large execution.
+		 * Such as while the condition is true, carry on executing. 
+		 */
+		boolean Boolean = true;
+		while(Boolean) // while counter is true, execute the code.
+		{
+			
+			System.out.println("This is a while loop ");
+			Boolean = false; // you have to make sure the while loop will change, as it can run forever!
+		}
 		
 		
 	}
