c5a0f0e7fc77c195b5205635eccdb319a33892e8
RattyMyles <up734233@myport.ac.uk>
2018-06-18 16:09:54 +0100

    Update AlphabetCh.java

--- Chapter 2/AlphabetCh.java
+++ Chapter 2/AlphabetCh.java
@@ -1,6 +1,6 @@
 /*
  *  Author: Rattymyles
- *  Chapter 1 Fundamentals of Java - A Beginner's Guide 6th Edition.
+ *  Chapter 2 Fundamentals of Java - A Beginner's Guide 6th Edition.
  *  The purpose of this project will breakdown each line of code to help fellow students and readers.
  *  The Alphabet program will demonstrate handling character variables similer to integers.
  *  Any feedback or comments, please email my address as m.pucknell@gmail.com.
